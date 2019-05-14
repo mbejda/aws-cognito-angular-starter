04da9a2113da70b70a025c7454892e416be983d4
Milos Bejda <mbejda@live.com>
2019-05-14 12:56:28 -0400

    save

---
+++ src/environments/environment.ts
@@ -0,0 +1,22 @@
+export const environment = {
+    production: false,
+
+    region: 'us-east-2',
+
+    identityPoolId: 'us-east-2:060f443f-9f15-42f2-8fdf-72937c22298d',
+    userPoolId: 'us-east-2_fLv4LfkC6',
+    clientId: '41stqu4gaaetd2021gd68s1rpo',
+
+    rekognitionBucket: 'rekognition-pics',
+    albumName: "usercontent",
+    bucketRegion: 'us-east-2',
+
+    ddbTableName: 'LoginTrailrltyapp',
+
+    cognito_idp_endpoint: '',
+    cognito_identity_endpoint: '',
+    sts_endpoint: '',
+    dynamodb_endpoint: '',
+    s3_endpoint: ''
+};
+
